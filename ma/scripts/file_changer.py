569f884ac9197f03c561d048cb86284ef557fef9
Peter <pm>
2020-02-03 15:54:09 +0100

    added files

--- ma/scripts/file_changer.py
+++ ma/scripts/file_changer.py
@@ -2,14 +2,28 @@
 
 output = ""
 
+f1 = open("test_video_list.txt", "r")
+f2 = open("video_segments.txt", "r")
+for line1 in f1:
+    for line2 in f2:
+        if line1[:11] == line2[:11]:
+            print("SAME-----------------------------------------")
+        else:
+            print(line1 + line2)
+        break
+f1.close()
+f2.close()
+"""
 with open('test_video_list.txt', 'r') as file1:
-    with open('test_segments', 'r') as file2:
+    with open('video_segments.txt', 'r') as file2:
         for line in file1:
+            print(line[:11])
             for check_line in file2:
+                print(check_line[:11])
                 if line[:11] == check_line[:11]:
                     line = line.split()
                     check_line = check_line.split()
-                    print(line[0])
+                    print(line)
 
         same = set(file1).intersection(file2)
 
@@ -18,5 +32,6 @@ same.discard('\n')
 with open('some_output_file.txt', 'w') as file_out:
     for line in same:
         file_out.write(line)
+"""
 
 print("finish")
