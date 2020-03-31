96c5b06dfe7f17f8d633617653ee7d8172866df4
Peter <pm>
2020-03-31 13:32:34 +0200

    added print json from npy file

---
+++ ma/scripts/20-03-04_save_files/show_json_of_npy_file.py
@@ -0,0 +1,35 @@
+import numpy as np
+import sys
+import json
+import os
+from pathlib import Path
+import psutil
+old = np.load
+np.load = lambda *a, **k: old(*a, **k, allow_pickle=True)
+
+path_to_numpy_files = Path(sys.argv[1])
+# path_to_numpy_files = Path(r"C:\Users\Asdf\Downloads\How2Sign_samples\openpose_output\json_testy_more_saved_numpy\\")
+numpy_files = [pos_json for pos_json in os.listdir(path_to_numpy_files) if pos_json.endswith('.npy')]
+
+process = psutil.Process(os.getpid())
+print("Current memory usage: %s megabytes" % str(process.memory_info().rss / 1000000))  # divided to get mb
+
+def load(path_to_numpy_file, file_name):
+    # load from .npy file
+    # print("load from %s" % path_to_numpy_file)
+    process = psutil.Process(os.getpid())
+    print("Current memory usage: %s megabytes" % str(process.memory_info().rss / 1000000))  # divided to get mb
+    all_files_dictionary = np.load(path_to_numpy_file).item()
+    print("Save to %s" % file_name)
+    process = psutil.Process(os.getpid())
+    print("Current memory usage: %s megabytes" % str(process.memory_info().rss / 1000000))  # divided to get mb
+    file_name = os.path.splitext(file_name)[0] + ".json"
+
+    jsonFile = open(file_name, "w+")
+    jsonFile.write(json.dumps(all_files_dictionary))
+    jsonFile.close()
+
+
+for file_name in numpy_files:
+    print("Loading %s" % file_name)
+    load(path_to_numpy_files / file_name, file_name)
